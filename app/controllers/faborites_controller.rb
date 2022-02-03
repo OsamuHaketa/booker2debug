c1316c95739ff89d2f7c8496c997f02221bf45d9
EC2 Default User <ec2-user@ip-172-31-17-7.ec2.internal>
2022-02-03 05:09:42 +0000

    いいね非同期実装

--- app/controllers/faborites_controller.rb
+++ app/controllers/faborites_controller.rb
@@ -1,16 +1,14 @@
 class FaboritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    faborite = current_user.faborites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    faborite = current_user.faborites.new(book_id: @book.id)
     faborite.save
-    redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    faborite = current_user.faborites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    faborite = current_user.faborites.find_by(book_id: @book.id)
     faborite.destroy
-    redirect_to request.referer
   end
 end
